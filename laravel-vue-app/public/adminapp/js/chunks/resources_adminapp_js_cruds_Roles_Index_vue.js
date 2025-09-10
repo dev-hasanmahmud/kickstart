719abfad55e2051470263826c8c5d9017097b121
dev-hasanmahmud <engr.hasanmahmud.bd@gmail.com>
2025-09-10 20:13:55 +0600

    H: kickstart with laravel and vue

---
+++ laravel-vue-app/public/adminapp/js/chunks/resources_adminapp_js_cruds_Roles_Index_vue.js
@@ -0,0 +1,13 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_adminapp_js_cruds_Roles_Index_vue"],{
+
+/***/ "./resources/adminapp/js/cruds/Roles/Index.vue":
+/*!*****************************************************!*\
+  !*** ./resources/adminapp/js/cruds/Roles/Index.vue ***!
+  \*****************************************************/
+/***/ (() => {
+
+throw new Error("Module parse failed: Unexpected token (1:0)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n> <template>\n|   <div class=\"container-fluid\">\n|     <div class=\"row\">");
+
+/***/ })
+
+}]);
