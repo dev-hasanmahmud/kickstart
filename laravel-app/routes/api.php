5842cf39a74f53fbb6699dc1eeb2afb7c3d3d06a
dev-hasanmahmud <engr.hasanmahmud.bd@gmail.com>
2025-09-10 17:18:58 +0600

    H: kickstart with laravel

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
 });
